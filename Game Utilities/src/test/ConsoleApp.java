d8e25bcac4f673de313c01231e1dee94848e0cba
Nageek15 <44506268+Nageek15@users.noreply.github.com>
2019-02-10 19:27:53 -0500

    Put project in repository

---
+++ Game Utilities/src/test/ConsoleApp.java
@@ -0,0 +1,15 @@
+package test;
+
+import gameutil.text.Console;
+import static gameutil.text.Console.theme;
+public class ConsoleApp {
+	public static void main(String[] argumentos){
+		Console console=new Console(theme.sea);
+		while(true){
+			console.readLineInt();
+			console.readLineDouble();
+			console.readLine();
+		}
+		
+	}
+}
--- src/gameutil/Line.java
+++
@@ -1,5 +0,0 @@
-package gameutil;
-
-public class Line {
-
-}
